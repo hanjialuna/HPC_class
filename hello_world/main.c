38f9c7ec5448e2f5e6d7873a8c1f720b2fd89246
hanjialuna <jialunhan2018@u.northwestern.edu>
2016-03-30 16:19:28 -0500

    add hellow world

---
+++ hello_world/main.c
@@ -0,0 +1,32 @@
+/* 
+	Hello World program in C language
+	
+	Program written by Jialun Han	
+	
+	Demonstration from class
+	
+	Inputs: none
+	
+	Outputs: write Hello world on terminal
+	
+	Editing History:
+	
+	30/03/2016: initial draft
+	
+*/
+
+#include <stdio.h>
+
+/* should insert comments on the main function
+*/
+
+int main (int argc, char* argv[])
+{
+	/* it is important to get in the habit of thoroughly commenting your code.*/
+	
+	printf("Hello World!\n");
+	
+	// all done return zero
+	
+	return 0;
+}
